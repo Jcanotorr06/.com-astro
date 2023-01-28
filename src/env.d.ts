d44edc16038c5fa9afa5db1be16502164f6bae51
Jcanotorr06 <jcano062001@gmail.com>
2023-01-27 19:28:50 -0500

    update: env.d.ts. configure new enviroment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+    readonly NOTION_TOKEN: string;
+    readonly EMAIL_SERVICE_ID: string;
+    readonly EMAIL_TEMPLATE_ID: string;
+    readonly EMAIL_PUBLIC_KEY: string;
+    readonly ASTRO_TEST_VAR: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
